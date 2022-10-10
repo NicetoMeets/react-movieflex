0593f39ee2c701b13263d1253916c948c4ccb1ef
Nice <puma1800@naver.com>
2022-10-11 02:02:05 +0900

    로우 후터 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
-import Banner from './componets/Banner';
-import Nav from './componets/Nav';
+import requests from './api/requests';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+import Row from './components/Row';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <div className='App'>
-      <Nav/>
-      <Banner/>
+    <div>
+      <Nav />
+      <Banner />
+      <Row
+      title='NETFLEX ORIGINALS'
+      id='NO'
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
+      <Row title='Top rated' id='TR' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' id='AM' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' id='CM' fetchUrl={requests.fetchComedyMovies} />
+      <Footer/>
     </div>
   )
 }
