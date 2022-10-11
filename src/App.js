01e781a3fa101907f4915cb21555cf994ea0e5da
Nice <puma1800@naver.com>
2022-10-11 19:37:36 +0900

    리액트 라우터 경로지정

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import './App.css';
-import requests from './api/requests';
-import Banner from './components/Banner';
 import Nav from './components/Nav';
-import Row from './components/Row';
 import Footer from './components/Footer';
+import { Outlet, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
 
-
-function App() {
+const Layout = () => {
   return (
     <div>
       <Nav />
-      <Banner />
-      <Row
-      title='NETFLEX ORIGINALS'
-      id='NO'
-      fetchUrl={requests.fetchNetflixOriginals}
-      isLargeRow
-      />
-      <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
-      <Row title='Top rated' id='TR' fetchUrl={requests.fetchTopRated} />
-      <Row title='Action Movies' id='AM' fetchUrl={requests.fetchActionMovies} />
-      <Row title='Comedy Movies' id='CM' fetchUrl={requests.fetchComedyMovies} />
-      <Footer/>
+
+      <Outlet />
+
+      <Footer />
     </div>
-  )
+  );
+};
+
+function App() {
+  return (
+    <div className="app">
+      <Routes> 
+        <Route path="/" element={<Layout />} >
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
+
 
 export default App;
